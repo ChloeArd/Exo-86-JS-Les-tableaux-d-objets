b32faae057bd8bc9645a7928aab0833730d1beab
ChloeArd <chloe.ardoise@gmail.com>
2020-12-15 15:49:36 +0100

    Exercice 86

--- app.js
+++ app.js
@@ -31,10 +31,18 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [citroen, ford, peugeot], // Vos voitures dans cette propriété !
 }
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
+for (let i = 0; i < voitures.collection.length; i++ ) {
+    citroen.ouvrirPorte();
+    ford.ouvrirPorte();
+    peugeot.ouvrirPorte();
+    let newSpan = document.createElement("span");
+    newSpan.innerHTML = "La couleur de la citoen est " + citroen.color + ", la couleur de la ford est " + ford.color + ", la couleur de la peugeot est  " + peugeot.color;
+    document.getElementById("couleurVoiture").appendChild(newSpan);
 
+}
 
